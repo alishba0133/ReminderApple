e960a561ae172c06f6a1a46df476f89557b4605d
Alishba <alishba.tahir@ucp.edu.pk>
2021-07-13 12:09:57 +0500

    Initial Commit

---
+++ ReminderApple/ReminderAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReminderAppleApp.swift
+//  ReminderApple
+//
+//  Created by ishba on 13/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ReminderAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
